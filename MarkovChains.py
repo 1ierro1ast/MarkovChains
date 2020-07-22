7b88a226868656a6fe451fe0a617adc6333a95f8
1ierro1ast <45067475+1ierro1ast@users.noreply.github.com>
2020-07-22 17:21:22 +0400

    Update MarkovChains.py

--- MarkovChains.py
+++ MarkovChains.py
@@ -23,20 +23,3 @@ class MarkovChains:
 				return out
 
 		return out
-
-mch = MarkovChains()
-test = "пчела ела кашу и запивала соком. Интересные вещи творятся повсюду. И самое главное что рыбы кашу не едят. Но важно учитывать погодные условия и прочие интересные вещи"
-test = test.replace(".", " * :").replace(",", "").lower().split(" ")
-for i, o in enumerate(test):
-	if i < len(test)-1:
-		mch.addChain(test[i], test[i+1])
-while True:
-	o = mch.generateSequence(10)
-	print(o)
-	if o[0] == "*" and o[len(o)-1] == ":":
-		break
-
-def filter(data):
-	return " ".join(data).replace(":", "." ).replace("*","").capitalize()
-
-print(filter(o))
